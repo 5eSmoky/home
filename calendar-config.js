55e3ef753c33eaf84422e26ab2c51b3656f5128e
Harpooter <hari4swami123@gmail.com>
2026-07-18 01:04:55 -0400

    name change

--- calendar-config.js
+++ calendar-config.js
@@ -1,6 +1,6 @@
 window.FES_CALENDAR_CONFIG = {
   availabilityUrl: "https://script.google.com/macros/s/AKfycbydHvZ8xAQ56pbJthOJM4UHQxZMa3IPlWxBm7mqoHJ-TNEbgo0pB_t5KLTWU028pBxqGQ/exec",
-  bookingApiUrl: "https://five-elements-smoky-bookings.five-elements-smoky-bookings.workers.dev",
+  bookingApiUrl: "https://five-elements-smoky-bookings.5esmokyrez.workers.dev",
   turnstileSiteKey: "0x4AAAAAAD4R87ZLcYQwLtQf",
   verificationEnabled: false,
 };
